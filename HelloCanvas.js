35220a3dda6c37c681f7c3d2444df9f92aec4c41
schand20 <schand20@uncc.edu>
2017-09-27 19:28:28 -0400

    -completed exercise 4b)iii - The World's...

--- HelloCanvas.js
+++ HelloCanvas.js
@@ -8,10 +8,17 @@ function main() {
   if (!gl) {
     console.log('Failed to get the rendering context for WebGL');
     return;
-  }
+}
+if (gl) {
+    console.log('GLContext successfully created');
+    return;
+}
+
+  
+
 
   // Set clear color
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.clearColor(1.0, 0.0, 0.0, 1.0);
 
   // Clear <canvas>
   gl.clear(gl.COLOR_BUFFER_BIT);
